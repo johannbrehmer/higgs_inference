b9eef15ce99dd6cdd07c4815e51c7bf77244b89a
Johann <mail@johannbrehmer.de>
2018-01-10 08:34:48 -0500

    Fixed bug in test sample generation

--- preprocessing/run_generate.sh
+++ preprocessing/run_generate.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=data
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=256GB
-#SBATCH --time=24:00:00
+#SBATCH --mem=64GB
+#SBATCH --time=12:00:00
 
 # Modules
 module purge
@@ -12,9 +12,9 @@ module load jupyter-kernels/py2.7
 module load scikit-learn/intel/0.18.1
 
 cd /home/jb6504/higgs_inference/preprocessing
-python -u generate_data.py train
-python -u generate_data.py basis
-python -u generate_data.py random
-python -u generate_data.py point-by-point
-python -u generate_data.py calibration
+#python -u generate_data.py train
+#python -u generate_data.py basis
+#python -u generate_data.py random
+#python -u generate_data.py point-by-point
+#python -u generate_data.py calibration
 python -u generate_data.py test
