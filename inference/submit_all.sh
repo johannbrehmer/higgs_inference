e269c741c7fe60699efa1ff47536befadf08c3c1
Johann <mail@johannbrehmer.de>
2018-01-16 08:20:21 -0500

    Restarted jobs due to carl version mismatch

--- inference/submit_all.sh
+++ inference/submit_all.sh
@@ -5,14 +5,20 @@
 sbatch run_carl_point_by_point.sh
 sbatch run_regression_point_by_point.sh
 
+sleep 60
+
 sbatch run_carl.sh
 sbatch run_combined.sh
 sbatch run_regression.sh
 sbatch run_score.sh
 sbatch run_combinedregression.sh
 
+sleep 60
+
 sbatch run_carl_aware.sh
 sbatch run_regression_aware.sh
+
+sleep 60
 
 sbatch run_carl_random.sh
 sbatch run_combined_random.sh
