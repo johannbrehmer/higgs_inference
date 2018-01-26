73d77b7226ce4a57c4ed450f336d3b664ca16d44
Johann <mail@johannbrehmer.de>
2018-01-26 13:39:04 -0500

    Minor changes

--- preprocessing/run_debug.sh
+++ preprocessing/run_debug.sh
@@ -12,4 +12,4 @@ module load jupyter-kernels/py2.7
 module load scikit-learn/intel/0.18.1
 
 cd /home/jb6504/higgs_inference/preprocessing
-python -u generate_data.py train basis random point-by-point calibration test debug
+python -u generate_data.py --neyman --debug
