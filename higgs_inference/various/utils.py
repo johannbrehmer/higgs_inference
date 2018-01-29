3c394c6d71dd1fff5c2aefe48d94d5366570c8de
Johann Brehmer <mail@johannbrehmer.de>
2018-01-29 17:07:59 -0500

    Cleaning various.utils

--- higgs_inference/various/utils.py
+++ higgs_inference/various/utils.py
@@ -1,15 +1,27 @@
+################################################################################
+# Imports
+################################################################################
+
 from __future__ import absolute_import, division, print_function
 
 from keras.layers import Dense, Dropout
 
 
+################################################################################
+# Translation between optimal decision function s and likelihood ratio r
+################################################################################
+
 def s_from_r(r):
-    return 1./(1. + r)
+    return 1. / (1. + r)
 
 
 def r_from_s(s, epsilon=1.e-3):
-    return (1. - s + epsilon)/(s + epsilon)
+    return (1. - s + epsilon) / (s + epsilon)
 
+
+################################################################################
+# Helper functions to build fully connected hidden layers
+################################################################################
 
 def stack_layers(layers):
     def f(x):
@@ -35,3 +47,32 @@ def build_hidden_layers(n,
             s = stack_layers([Dense(hidden_layer_size, activation=activation)])
         r.append(s)
     return stack_layers(r)
+
+
+################################################################################
+# Formatting numbers as output strings
+################################################################################
+
+def format_number(number,
+                  precision=2,
+                  trailing_zeros=True,
+                  fix_minus_zero=True,
+                  latex_math_mode=False,
+                  emphasize=False):
+    if precision == 0:
+        temp = str(int(round(number, precision)))
+    elif trailing_zeros:
+        temp = ('{:.' + str(precision) + 'f}').format(round(number, precision))
+    else:
+        temp = str(round(number, precision))
+    if fix_minus_zero and len(temp) > 0:
+        if temp[0] == '-' and float(temp) == 0.:
+            temp = temp[1:]
+    if latex_math_mode:
+        if emphasize:
+            temp = '$\mathbf{' + temp + '}$'
+        else:
+            temp = '$' + temp + '$'
+    elif emphasize:
+        temp = r'\emph{' + temp + r'}'
+    return temp
