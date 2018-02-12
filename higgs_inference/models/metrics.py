2ea7aa77de633add1b7579aa2a830fd82cfeef07
Johann Brehmer <mail@johannbrehmer.de>
2018-02-12 17:04:10 -0500

    More work on trimmed metrics

--- higgs_inference/models/metrics.py
+++ higgs_inference/models/metrics.py
@@ -19,11 +19,12 @@ def trimmed_cross_entropy(y_true, y_pred):
 
     # Trim at bottom and then at top
     if cross_entropies.shape is not None:
-        if cross_entropies.shape[0].value is not None:
-            _, top_indices = tf.nn.top_k(cross_entropies, settings.trim_mean_absolute)
-            _, bottom_indices = tf.nn.top_k(- cross_entropies, settings.trim_mean_absolute)
-            cross_entropies[top_indices] = 0.
-            cross_entropies[bottom_indices] = 0.
+        if cross_entropies.shape.ndims >= 1:
+            if cross_entropies.shape[0].value is not None:
+                _, top_indices = tf.nn.top_k(cross_entropies, settings.trim_mean_absolute)
+                _, bottom_indices = tf.nn.top_k(- cross_entropies, settings.trim_mean_absolute)
+                cross_entropies[top_indices] = 0.
+                cross_entropies[bottom_indices] = 0.
 
     # Mean
     return cross_entropies
@@ -35,11 +36,12 @@ def trimmed_mse_log_r(y_true, y_pred):
 
     # Set loss for top and bottom indices to zero
     if mse.shape is not None:
-        if mse.shape[0].value is not None:
-            _, top_indices = tf.nn.top_k(mse, settings.trim_mean_absolute)
-            _, bottom_indices = tf.nn.top_k(- mse, settings.trim_mean_absolute)
-            mse[top_indices] = 0.
-            mse[bottom_indices] = 0.
+        if mse.shape.ndims >= 1:
+            if mse.shape[0].value is not None:
+                _, top_indices = tf.nn.top_k(mse, settings.trim_mean_absolute)
+                _, bottom_indices = tf.nn.top_k(- mse, settings.trim_mean_absolute)
+                mse[top_indices] = 0.
+                mse[bottom_indices] = 0.
 
     # Mean
     return mse
@@ -51,11 +53,12 @@ def trimmed_mse_score(y_true, y_pred):
 
     # Set loss for top and bottom indices to zero
     if mse.shape is not None:
-        if mse.shape[0].value is not None:
-            _, top_indices = tf.nn.top_k(mse, settings.trim_mean_absolute)
-            _, bottom_indices = tf.nn.top_k(- mse, settings.trim_mean_absolute)
-            mse[top_indices] = 0.
-            mse[bottom_indices] = 0.
+        if mse.shape.ndims >= 1:
+            if mse.shape[0].value is not None:
+                _, top_indices = tf.nn.top_k(mse, settings.trim_mean_absolute)
+                _, bottom_indices = tf.nn.top_k(- mse, settings.trim_mean_absolute)
+                mse[top_indices] = 0.
+                mse[bottom_indices] = 0.
 
     # Mean
     return mse
