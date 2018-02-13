99eb2e3bdde68e925f030320f26bcb79c3dada19
Johann Brehmer <mail@johannbrehmer.de>
2018-02-12 21:01:31 -0500

    Switch trimming to only min and max, no reliance on tf.nn.k_max any more

--- higgs_inference/models/metrics.py
+++ higgs_inference/models/metrics.py
@@ -5,6 +5,7 @@
 import logging
 
 import tensorflow as tf
+from keras import backend as K
 
 from higgs_inference import settings
 from higgs_inference.models.loss_functions import loss_function_carl, loss_function_ratio_regression, \
@@ -19,13 +20,15 @@ def trimmed_cross_entropy(y_true, y_pred):
     # Calculate cross entropies
     cross_entropies = loss_function_carl(y_true, y_pred)
 
+    cross_entropies = cross_entropies - K.max(cross_entropies) / 32 - K.min(cross_entropies) / 32
+
     # Trim at bottom and then at top
     #if cross_entropies.shape is not None:
     #    if cross_entropies.shape.ndims >= 1:
     #        if cross_entropies.shape[0].value is not None:
-    _, top_indices = tf.nn.top_k(cross_entropies, 2)
+    #_, top_indices = tf.nn.top_k(cross_entropies, 2)
     #_, bottom_indices = tf.nn.top_k(- cross_entropies, 2)
-    cross_entropies[top_indices] = 0.
+    #cross_entropies[top_indices] = 0.
     #cross_entropies[bottom_indices] = 0.
 
     #            logging.debug('CE values: %s %s %s', cross_entropies, top_indices, bottom_indices)
@@ -38,7 +41,6 @@ def trimmed_cross_entropy(y_true, y_pred):
     #else:
     #    logging.debug('CE shapes: %s', cross_entropies.shape)
 
-    # Mean
     return cross_entropies
 
 
@@ -46,14 +48,16 @@ def trimmed_mse_log_r(y_true, y_pred):
     # Calculate MSE
     mse = loss_function_ratio_regression(y_true, y_pred)
 
-    # Set loss for top and bottom indices to zero
-    if mse.shape is not None:
-        if mse.shape.ndims >= 1:
-            if mse.shape[0].value is not None:
-                _, top_indices = tf.nn.top_k(mse, settings.trim_mean_absolute)
-                _, bottom_indices = tf.nn.top_k(- mse, settings.trim_mean_absolute)
-                mse[top_indices] = 0.
-                mse[bottom_indices] = 0.
+    mse = mse - K.max(mse) / 32 - K.min(mse) / 32
+
+    # # Set loss for top and bottom indices to zero
+    # if mse.shape is not None:
+    #     if mse.shape.ndims >= 1:
+    #         if mse.shape[0].value is not None:
+    #             _, top_indices = tf.nn.top_k(mse, settings.trim_mean_absolute)
+    #             _, bottom_indices = tf.nn.top_k(- mse, settings.trim_mean_absolute)
+    #             mse[top_indices] = 0.
+    #             mse[bottom_indices] = 0.
 
     # Mean
     return mse
@@ -63,14 +67,16 @@ def trimmed_mse_score(y_true, y_pred):
     # Calculate cross entropies
     mse = loss_function_score(y_true, y_pred)
 
-    # Set loss for top and bottom indices to zero
-    if mse.shape is not None:
-        if mse.shape.ndims >= 1:
-            if mse.shape[0].value is not None:
-                _, top_indices = tf.nn.top_k(mse, settings.trim_mean_absolute)
-                _, bottom_indices = tf.nn.top_k(- mse, settings.trim_mean_absolute)
-                mse[top_indices] = 0.
-                mse[bottom_indices] = 0.
+    mse = mse - K.max(mse) / 32 - K.min(mse) / 32
+
+    # # Set loss for top and bottom indices to zero
+    # if mse.shape is not None:
+    #     if mse.shape.ndims >= 1:
+    #         if mse.shape[0].value is not None:
+    #             _, top_indices = tf.nn.top_k(mse, settings.trim_mean_absolute)
+    #             _, bottom_indices = tf.nn.top_k(- mse, settings.trim_mean_absolute)
+    #             mse[top_indices] = 0.
+    #             mse[bottom_indices] = 0.
 
     # Mean
     return mse
