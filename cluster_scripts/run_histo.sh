762e7ee333e5c6d10002f00a4c7f97449a32005b
Johann <mail@johannbrehmer.de>
2018-03-04 21:58:36 -0500

    Started jobs with new training samples

--- cluster_scripts/run_histo.sh
+++ cluster_scripts/run_histo.sh
@@ -12,9 +12,9 @@
 module purge
 module load jupyter-kernels/py2.7
 module load scikit-learn/intel/0.18.1
-module load theano/0.9.0
-module load tensorflow/python2.7/20170707
-module load keras/2.0.2
+# module load theano/0.9.0
+# module load tensorflow/python2.7/20170707
+# module load keras/2.0.2
 
 cd /home/jb6504/higgs_inference/higgs_inference
 
