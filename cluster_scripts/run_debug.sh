9a0c6b998f04f12acb5126472a5ff7eb132aeb5c
Johann <mail@johannbrehmer.de>
2018-03-22 11:22:52 -0400

    New results

--- cluster_scripts/run_debug.sh
+++ cluster_scripts/run_debug.sh
@@ -18,4 +18,4 @@ module load keras/2.0.2
 
 cd /home/jb6504/higgs_inference/higgs_inference
 
-python -u experiments.py combinedregression -o debug deep new
+python -u experiments.py combinedregression -o short deep new
