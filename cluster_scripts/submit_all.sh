e2564bab0baf6bd468f0531b1c50a31e186e48ea
Johann <mail@johannbrehmer.de>
2018-03-26 17:55:50 -0400

    Started new jobs

--- cluster_scripts/submit_all.sh
+++ cluster_scripts/submit_all.sh
@@ -8,18 +8,18 @@ cd /home/jb6504/higgs_inference/cluster_scripts
 
 # sbatch run_truth.sh
 
-sbatch run_histo.sh
+# sbatch run_histo.sh
 # sbatch run_afc.sh
 
-sbatch run_scoreregression.sh
+# sbatch run_scoreregression.sh
 
 # sbatch run_carl_point_by_point.sh
 # sbatch run_regression_point_by_point.sh
 
-sbatch run_carl.sh
-sbatch run_combined.sh
-sbatch run_regression.sh
-sbatch run_combinedregression.sh
+# sbatch run_carl.sh
+# sbatch run_combined.sh
+# sbatch run_regression.sh
+# sbatch run_combinedregression.sh
 
 # sbatch run_carl_aware.sh
 # sbatch run_combined_aware.sh
@@ -31,9 +31,9 @@ sbatch run_combinedregression.sh
 # Diagnostics
 ################################################################################
 
-sbatch --array=1-4 run_truth_diagnostics.sh
+# sbatch --array=1-4 run_truth_diagnostics.sh
 
-sbatch --array=1-4 run_histo_diagnostics.sh
+# sbatch --array=1-4 run_histo_diagnostics.sh
 sbatch --array=1-4 run_scoreregression_diagnostics.sh
 
 sbatch --array=1-4 run_carl_diagnostics.sh
@@ -59,8 +59,8 @@ sbatch --array=1-4 run_combinedregression_diagnostics.sh
 # Hyperparameter scans
 ################################################################################
 
-sbatch run_combined_tuning.sh
-sbatch run_combinedregression_tuning.sh
+# sbatch run_combined_tuning.sh
+# sbatch run_combinedregression_tuning.sh
 
 # sbatch run_carl_depth_experiments.sh
 # sbatch run_combined_depth_experiments.sh
