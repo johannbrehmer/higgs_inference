79ee3cca1760185d25a7f1caadc50e848c3e2c7e
Johann Brehmer <mail@johannbrehmer.de>
2018-03-29 17:55:56 -0400

    Prepared new smearing runs

--- cluster_scripts/submit_all.sh
+++ cluster_scripts/submit_all.sh
@@ -11,7 +11,7 @@ cd /home/jb6504/higgs_inference/cluster_scripts
 # sbatch run_histo.sh
 # sbatch run_afc.sh
 
-sbatch run_scoreregression.sh
+# sbatch run_scoreregression.sh
 
 # sbatch run_carl_point_by_point.sh
 # sbatch run_regression_point_by_point.sh
@@ -34,7 +34,7 @@ sbatch run_scoreregression.sh
 # sbatch --array=1-4 run_truth_diagnostics.sh
 
 # sbatch --array=1-4 run_histo_diagnostics.sh
-sbatch --array=1-4 run_scoreregression_diagnostics.sh
+# sbatch --array=1-4 run_scoreregression_diagnostics.sh
 
 # sbatch --array=1-4 run_carl_diagnostics.sh
 # sbatch --array=1-4 run_combined_diagnostics.sh
@@ -46,13 +46,13 @@ sbatch --array=1-4 run_scoreregression_diagnostics.sh
 # Smearing
 ################################################################################
 
-# sbatch run_histo_smearing.sh
-# sbatch run_scoreregression_smearing.sh
+sbatch run_histo_smearing.sh
+sbatch run_scoreregression_smearing.sh
 
-# sbatch run_carl_smearing.sh
-# sbatch run_combined_smearing.sh
-# sbatch run_regression_smearing.sh
-# sbatch run_combinedregression_smearing.sh
+sbatch run_carl_smearing.sh
+sbatch run_combined_smearing.sh
+sbatch run_regression_smearing.sh
+sbatch run_combinedregression_smearing.sh
 
 
 ################################################################################
