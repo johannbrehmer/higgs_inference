1b3dc8964288c19c7d0754633c497d2654da9867
Johann <mail@johannbrehmer.de>
2018-04-02 21:12:08 -0400

    Started new jobs after cluster shutdown

--- cluster_scripts/submit_all.sh
+++ cluster_scripts/submit_all.sh
@@ -11,7 +11,7 @@ cd /home/jb6504/higgs_inference/cluster_scripts
 # sbatch run_histo.sh
 # sbatch run_afc.sh
 
-# sbatch run_scoreregression.sh
+sbatch run_scoreregression.sh
 
 # sbatch run_carl_point_by_point.sh
 # sbatch run_regression_point_by_point.sh
