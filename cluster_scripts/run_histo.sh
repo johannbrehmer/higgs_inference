5af457a1df6b40a5905e195851a543fcf4e3fdaf
Johann <mail@johannbrehmer.de>
2018-04-04 10:04:11 -0400

    Started last (?) remaining jobs

--- cluster_scripts/run_histo.sh
+++ cluster_scripts/run_histo.sh
@@ -18,6 +18,6 @@ module load scikit-learn/intel/0.18.1
 
 cd /home/jb6504/higgs_inference/higgs_inference
 
-python -u experiments.py histo -x 1 41 #--neyman -o neyman2
-python -u experiments.py histo -x 1
-python -u experiments.py histo -x 41
+python -u experiments.py histo -x 1 41 --neyman -o neyman2
+# python -u experiments.py histo -x 1
+# python -u experiments.py histo -x 41
