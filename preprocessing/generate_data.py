b935e0d766647591e27ad61b784df43fcf733340
Johann Brehmer <mail@johannbrehmer.de>
2018-04-26 10:31:01 -0400

    More plots

--- preprocessing/generate_data.py
+++ preprocessing/generate_data.py
@@ -482,7 +482,7 @@ if args.pointbypoint:
             cut]
 
 
-    for t in range(125, settings.n_thetas):  # enumerate(settings.extended_pbp_training_thetas):
+    for t in range(125):
         this_th0, this_th1, this_X, this_y, this_scores, this_r, this_p0, this_p1 = generate_data_train_point_by_point(
             t, theta1)
 
